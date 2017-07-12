64304fd271e5a34cce1dab35930dc11361235b9b
Jordan Estes <jle48@georgetown.edu>
2017-07-12 15:05:42 -0400

    Isolated values

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,7 @@
-// your code goes here ...
+function validate() {
+  // Retrieve values from age and relationship input fields and store them in variables
+  var checkAge = document.getElementById('age').value;
+  var checkRelation = document.getElementById('relationship').value;
+
+  
+}
